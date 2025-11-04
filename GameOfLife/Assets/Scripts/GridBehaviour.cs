cd01f84772098cd367e67772251a6e283df8b148
Mitchell R <Morobinson2608@gmail.com>
2025-11-04 18:29:23 +0000

    Fixed terrible lag.

--- GameOfLife/Assets/Scripts/GridBehaviour.cs
+++ GameOfLife/Assets/Scripts/GridBehaviour.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Tilemaps;
+
 public class GridBehaviour : MonoBehaviour
 {
+
+    private readonly HashSet<Vector3Int> aliveCells = new();
+    private readonly HashSet<Vector3Int> cellsToCheck = new();
     [SerializeField] private Tilemap currentState;
     [SerializeField] private Tilemap nextState;
     [SerializeField] private Tile aliveTile;
@@ -10,7 +18,9 @@ public class GridBehaviour : MonoBehaviour
     [SerializeField] private float updateInterval = 0.05f;
 
 
-    private void Start()
+
+
+    private void Awake()
     {
         SetPattern(pattern);
     }
@@ -19,16 +29,113 @@ public class GridBehaviour : MonoBehaviour
     {
         Clear();
 
+        Vector2Int center = pattern.GetCenter();
+
         for (int i = 0; i < pattern.cells.Length; i++)
         {
-            Vector2Int cell = pattern.cells[i];
-            currentState.SetTile(new Vector3Int(cell.x, cell.y, 0), aliveTile);
+            Vector3Int cell = (Vector3Int)(pattern.cells[i] - center);
+            currentState.SetTile(cell, aliveTile);
+            aliveCells.Add(cell);
         }
+
+        Debug.Log($"Pattern has been set. There are {aliveCells.Count} cells alive in this pattern.");
+
     }
 
     private void Clear()
     {
+        aliveCells.Clear();
+        cellsToCheck.Clear();
         currentState.ClearAllTiles();
         nextState.ClearAllTiles();
     }
+
+
+    private void OnEnable()
+    {
+        StartCoroutine(Simulate());
+    }
+
+    private IEnumerator Simulate()
+    {
+        var interval = new WaitForSeconds(updateInterval);
+        yield return interval;
+
+        while (enabled)
+        {
+            UpdateState();
+
+            yield return interval;
+        }
+    }
+
+    private void UpdateState()
+    {
+        cellsToCheck.Clear();
+
+        Debug.Log("Updating state...");
+        foreach (Vector3Int cell in aliveCells)
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    cellsToCheck.Add(cell + new Vector3Int(x, y));
+                }
+            }
+        }
+
+        Debug.Log($"Finished calculating cells to check. There are {cellsToCheck.Count} for this cycle");
+
+
+        foreach (Vector3Int cell in cellsToCheck)
+        {
+            int neighbours = 0;
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    Vector3Int neighbour = cell + new Vector3Int(x, y);
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
+                    if (currentState.GetTile(neighbour) == aliveTile)
+                    {
+                        neighbours++;
+                    }
+                }
+            }
+
+            bool isAlive = currentState.GetTile(cell) == aliveTile;
+
+            Debug.Log($"Alive ? {isAlive}");
+
+            if ((neighbours < 2 || neighbours > 3) && isAlive)
+            {
+                nextState.SetTile(cell, jedTile);
+                aliveCells.Remove(cell);
+
+            }
+            else if (!isAlive && neighbours == 3)
+            {
+                nextState.SetTile(cell, aliveTile);
+                aliveCells.Add(cell);
+            }
+            else
+            {
+                nextState.SetTile(cell, currentState.GetTile(cell));
+            }
+        }
+
+        Debug.Log($"Finished calculating alive / dead cells for next cyle there are {aliveCells.Count} for the next cycle");
+
+        Tilemap temp = currentState;
+        currentState = nextState;
+        nextState = temp;
+        nextState.ClearAllTiles();
+
+    }
+
 }
